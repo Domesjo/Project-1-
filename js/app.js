712602dee34c45153f1fc47e6c8558cd44a93f5b
Sam Domesjö <samdomesjo@gmail.com>
2017-02-10 16:38:33 +0000

    some bugs still to fix

--- js/app.js
+++ js/app.js
@@ -1,1 +1,53 @@
-console.log('JS loaded');
+$(()=>{
+
+//Fix reset
+//fix addRandomMole
+  console.log('JS loaded');
+  const $board = $('#container');
+
+  //const $targets = $('div');
+  let score = 0;
+  const $startBtn = $('.play');
+
+  function createBoard(){
+    for(let i=0;i<30;i++){
+      $board.append('<div>');
+      $('div').addClass('grid');
+      $startBtn.removeClass('grid');
+    }
+  }
+
+
+  function addRandomMole(){
+    const mole = Math.floor(Math.random()*$('div').length);
+    $('.grid').eq(mole).addClass('mole');
+    //Mole måste va i randomMole för att hoistas till rätt tillfälle
+    $('.mole').click((e)=>{
+      $(e.target).removeClass('mole');
+      score++;
+      console.log(score);
+    });
+
+  }
+
+  function start(){
+    const timeId = setInterval(()=>{
+      for (let i=0;i<3;i++){
+        addRandomMole();
+      }
+    },1000);
+    setTimeout(()=>{
+      clearInterval(timeId);
+    },10000);
+
+  }
+
+
+
+  $startBtn.click(function gameTime(){
+    createBoard();
+    start();
+
+  });
+
+});

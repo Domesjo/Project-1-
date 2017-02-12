ea12059a03885f68430b2f5c9e4fc82fa22592b1
Sam Domesjö <samdomesjo@gmail.com>
2017-02-12 17:53:58 +0000

    did some refactoring and added the Instructions div

--- js/app.js
+++ js/app.js
@@ -1,28 +1,61 @@
+function timeOut(num,toRemove,time){
+  setTimeout(()=> {
+    $('.grid').eq(num).removeClass(toRemove);
+  },time);
+}
+function CalcRandom(){
+  return  Math.floor(Math.random()*$('div').length);
+}
+let time = 10;
+let score = 0;
+
+function addRandomMole(){
+  const mole = CalcRandom();
+  $('.grid').eq(mole).addClass('mole');
+  //Mole måste va i randomMole för att hoistas till rätt tillfälle
+  timeOut(mole,'mole',2000);
+}
+
+function addRandomTree(){
+  const tree = CalcRandom();
+  ($('.grid').hasClass('mole')) ? $('.grid').eq(tree).addClass('tree') : console.log('dont work');
+  timeOut(tree,'tree',1000);
+}
+
+function addRandomBad(){
+  const bad = CalcRandom();
+  ($('.grid').hasClass('tree')) ? $('.grid').eq(bad).addClass('bad') : console.log('bad dont work');
+
+  timeOut(bad,'bad',1750);
+}
 $(()=>{
 
-//Fix reset
-//fix addRandomMole
   console.log('JS loaded');
   const $board = $('#container');
   const $activeScore = $('.score');
-  //const $targets = $('div');
-  let time = 10;
-  let score = 0;
   const $startBtn = $('.play');
   const $reset = $('.reset');
   const $timer = $('.time');
+  const $instructions = $('.Instructions');
+  const $how= $('.how');
   //const $grid = $('.grid'); funkar ej av någon konstig anledning
 
   $reset.hide();
 
+  function removeGrid(){
+    $startBtn.removeClass('grid');
+    $timer.removeClass('grid');
+    $instructions.removeClass('grid');
+    $reset.removeClass('grid');
+    $how.removeClass('grid');
+  }
 
   function createBoard(){
     $startBtn.hide();
     for(let i=0;i<30;i++){
       $board.append('<div>');
       $('div').addClass('grid');
-      $startBtn.removeClass('grid');
-      $timer.removeClass('grid');
+      removeGrid();
     }
   }
 
@@ -30,35 +63,12 @@ $(()=>{
     $activeScore.text(score);
   }
 
-  function CalcRandom(){
-    return  Math.floor(Math.random()*$('div').length);
-  }
-
-  function addRandomMole(){
-    const mole = CalcRandom();
-    $('.grid').eq(mole).addClass('mole');
-    //Mole måste va i randomMole för att hoistas till rätt tillfälle
-    setTimeout(()=>{
-      $('.grid').eq(mole).removeClass('mole');
-    },2000);
-  }
-
-  function addRandomTree(){
-    const tree = CalcRandom();
-    ($('.grid').hasClass('mole')) ? $('.grid').eq(tree).addClass('tree') : console.log('dont work');
-
-    setTimeout(()=>{
-      $('.grid').eq(tree).removeClass('tree');
-    },1000);
-  }
-
-  function addRandomBad(){
-    const bad = CalcRandom();
-    ($('.grid').hasClass('tree')) ? $('.grid').eq(bad).addClass('bad') : console.log('bad dont work');
-
-    setTimeout(()=> {
-      $('.grid').eq(bad).removeClass('bad');
-    },1750);
+  function targeter (e,target,scoretoAdd){
+    if($(e.target).hasClass(target)){
+      $(e.target).removeClass(target);
+      score+=scoretoAdd;
+      console.log('works');
+    }
   }
 
 
@@ -67,21 +77,29 @@ $(()=>{
       addRandomMole();
       upDateScore();
       $('.displayTime').text(time);
-    },330);
+    },500);
+
     const tidId = setInterval(()=>{
       time--;
       addRandomTree();
       addRandomBad();
     },1000);
+
     setTimeout(()=>{
       clearInterval(timeId);
       clearInterval(tidId);
       $reset.show();
       $('.grid').removeClass('mole');
-    },11000);
+    },10500);
 
   }
 
+  $instructions.click(()=>{
+    $how.show();
+    $how.click(()=>{
+      $how.hide();
+    });
+  });
 
   $reset.click(()=>{
     $reset.hide();
@@ -90,30 +108,19 @@ $(()=>{
     score = 0;
     time = 10;
     $activeScore.text(score);
-    console.log('hey');
   });
 
   $startBtn.click(function gameTime(){
     createBoard();
     start();
     $('.grid').click((e)=>{
-      if($(e.target).hasClass('mole')){
-        $(e.target).removeClass('mole');
-        score++;
-        console.log(score);
-      }
+      targeter(e,'mole',1);
     });
     $('.grid').click((e)=>{
-      if($(e.target).hasClass('tree')){
-        $(e.target).removeClass('tree');
-        score+=2;
-      }
+      targeter(e,'tree',2);
     });
     $('.grid').click((e)=>{
-      if($(e.target).hasClass('bad')){
-        $(e.target).removeClass('bad');
-        score--;
-      }
+      targeter(e,'bad',-1);
     });
 
   });

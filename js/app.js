5d5ac23a0067eccaf4c5746c6869628fa3ee961a
Sam Domesjö <samdomesjo@gmail.com>
2017-02-14 11:43:26 +0000

    Refactored, no known bugs yet

--- js/app.js
+++ js/app.js
@@ -14,9 +14,11 @@ let score = 0;
 
 $(()=>{
 //  let time = 15;
+  let $grid;
+  let tidId;
   let time = parseInt($('select option:selected').val());
   let difficulty = 2;
-
+  let timeId;
   const hiScore4 = localStorage.getItem('hiScore4');
   const hiScore3 = localStorage.getItem('hiScore3');
   const hiScore2 = localStorage.getItem('hiScore2');
@@ -30,27 +32,21 @@ $(()=>{
   const $how= $('.how');
   const $cash = $('.cash');
 
-  $('#1').text(localStorage.getItem('hiScore'));
-  $('#2').text(localStorage.getItem('hiScore2'));
-  $('#3').text(localStorage.getItem('hiScore3'));
-  $('#4').text(localStorage.getItem('hiScore4'));
-  $reset.hide();
-
-  function removeGrid(){
-    $startBtn.removeClass('grid');
-    $timer.removeClass('grid');
-    $instructions.removeClass('grid');
-    $reset.removeClass('grid');
-    $how.removeClass('grid');
-    $('.highScore').removeClass('grid');
+  function checkHighScore(){
+    $('#1').text(localStorage.getItem('hiScore'));
+    $('#2').text(localStorage.getItem('hiScore2'));
+    $('#3').text(localStorage.getItem('hiScore3'));
+    $('#4').text(localStorage.getItem('hiScore4'));
   }
+
+  $reset.hide();
+  checkHighScore();
 
   function createBoard(){
     $startBtn.hide();
     for(let i=0;i<30;i++){
-      $board.append('<div>');
-      $('div').addClass('grid');
-      removeGrid();
+      $board.append($('<div/>', { class: 'grid' }));
+      $grid = $('.grid');
     }
   }
 
@@ -58,48 +54,60 @@ $(()=>{
     $activeScore.text(score);
   }
 
-  function targeter (e,target,scoretoAdd){
-    if($(e.target).hasClass(target)){
-      $(e.target).removeClass(target);
-      score+=scoretoAdd;
-      playSound();
-      console.log('works');
-    }
+  const scores = {
+    mole: 1,
+    tree: 2,
+    'mole tree': 3,
+    bad: -1,
+    'grid': 0,
+    'mole bad ': 0,
+    'bad mole': 0
+  };
+
+  function targeter (e){
+    const targetClass = $(e.target).attr('class').replace(/^grid /, '');
+    console.log(targetClass);
+    const scoreToAdd = scores[targetClass];
+    if(targetClass==='tree'||targetClass==='mole'|| targetClass ==='bad')
+      $(e.target).removeClass(targetClass);
+    score += scoreToAdd;
+    playSound();
   }
   function playSound() {
     $cash.trigger('play');
   }
 
+  function isHighScore(prevHiscore,presentHiscore,hiScoreToSet){
+    if(score < prevHiscore && score > presentHiscore) localStorage.setItem(hiScoreToSet, score);
+  }
+
   function highScore(){
     if(hiScore !== null) {
       if(score > hiScore ) localStorage.setItem('hiScore', score);
-
-      else if(hiScore2 !== null){
-        if(score < hiScore && score > hiScore3) localStorage.setItem('hiScore2',score);
-      } else if(hiScore3 !== null){
-        if(score < hiScore2 && score > hiScore3) localStorage.setItem('hiScore3',score);
-      }else if(hiScore4 !== null){
-        if(score < hiScore3 && score > hiScore4) localStorage.setItem('hiScore4',score);
-      }
+      isHighScore(hiScore,hiScore3,'hiScore2');
+      isHighScore(hiScore2,hiScore3,'hiScore3');
+      isHighScore(hiScore3,hiScore4,'hiScore4');
     }
   }
-  function start(){
-    difficulty = parseInt($('#difficultySelector option:selected').val());
-    time = parseInt($('#selector option:selected').val());
-    const timeId = setInterval(()=>{
+
+  function moleGenerator(){
+    timeId = setInterval(()=>{
       addRandomMole();
       upDateScore();
       $('.displayTime').text(time);
+      return timeId;
     },500);
-
-    const tidId = setInterval(()=>{
+  }
+  function timeCounter(){
+    tidId = setInterval(()=>{
       time--;
       addRandomTree();
       addRandomBad();
+      return tidId;
     },1000);
-
+  }
+  function finish(){
     setTimeout(()=>{
-
       clearInterval(timeId);
       clearInterval(tidId);
       $reset.show();
@@ -107,34 +115,40 @@ $(()=>{
       highScore();
       localStorage.getItem('hiScore');
     },time*1000+500);
-
   }
 
+  function start(){
+    difficulty = parseInt($('#difficultySelector option:selected').val());
+    time = parseInt($('#selector option:selected').val());
+    moleGenerator();
+    timeCounter();
+    finish();
+  }
+
+
+
   function addRandomMole(){
-    const mole = CalcRandom();
-    $('.grid').eq(mole).addClass('mole');
-    //Mole måste va i randomMole för att hoistas till rätt tillfälle
-    timeOut(mole,'mole',difficulty *1000 );
+    const num = CalcRandom();
+    $grid.eq(num).addClass('mole');
+    timeOut(num,'mole',difficulty *1000 );
   }
 
   function addRandomTree(){
-    const tree = CalcRandom();
-    ($('.grid').hasClass('mole')) ? $('.grid').eq(tree).addClass('tree') : console.log('dont work');
-    timeOut(tree,'tree',difficulty*1000/2);
+    const num = CalcRandom();
+    ($('div').hasClass('grid')) ? $grid.eq(num).addClass('tree'): console.log('wood');
+    timeOut(num,'tree',difficulty*1000/2);
   }
 
   function addRandomBad(){
-    const bad = CalcRandom();
-    ($('.grid').hasClass('tree')) ? $('.grid').eq(bad).addClass('bad') : console.log('bad dont work');
-
-    timeOut(bad,'bad',difficulty*1000 +500);
+    const num = CalcRandom();
+    ($('div').hasClass('grid')) ? $grid.eq(num).addClass('bad') : console.log('bad dont work');
+    timeOut(num,'bad',difficulty*1000 +500);
   }
 
   $instructions.click(()=>{
     $how.show();
     $how.click(()=>{
       $how.hide();
-      console.log(parseInt($('#difficultySelector option:selected').val()));
     });
   });
 
@@ -147,20 +161,13 @@ $(()=>{
     start();
   });
 
+
   $startBtn.click(function gameTime(){
     createBoard();
     setTimeout(()=>{
       start();
     },500);
-    $('.grid').click((e)=>{
-      targeter(e,'mole',1);
-    });
-    $('.grid').click((e)=>{
-      targeter(e,'tree',2);
-    });
-    $('.grid').click((e)=>{
-      targeter(e,'bad',-1);
-    });
+    $('.grid').click(targeter);
 
   });
 
